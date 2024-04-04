8f08ece45a7fd4dd00f71d99fd1e87456b0fc9ac
RICCOM <ericomun@gmail.comgit>
2024-04-04 16:29:39 +0300

    adds events event listening

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// const input = document.getElementById('button');
+// function addingEventListener() {
+//     function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
+
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+      alert('I was clicked!');
+    });
 }
+addingEventListener();
